e8748b650ddedb98cc0100e0c86726207d48911d
Salma Ahmed <55872989+salma247@users.noreply.github.com>
2021-09-18 20:30:11 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('fas fa-bars');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
